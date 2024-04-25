0ccfb999d51b85f30b1aa54b41a3f0a57bbbd95e
mats-gz <matiasguzmanrodriguez89@gmail.com>
2024-04-25 11:21:25 -0300

    ej terminado

--- Futbol.py
+++ Futbol.py
@@ -1,5 +1,5 @@
 class jugFutbol:
-    def __init__(self, nombre, edad, posicion, equipo, pais, numCamisa, goles, partidos, asistencias, tarRoja, tarAmarilla, cantPremios = 0, cantReconocimientos = 0, premios = {}):
+    def __init__(self, nombre, edad, posicion, equipo, pais, numCamisa, goles, partidos, asistencias, tarRoja, tarAmarilla, cantReconocimientos = 0, cantPremios = 0, premios = None):
         self.nombre = nombre
         self.edad = edad
         self.posicion = posicion
@@ -11,9 +11,9 @@ class jugFutbol:
         self.asistencias = asistencias
         self.tarRoja = tarRoja
         self.tarAmarilla = tarAmarilla
-        self.cantPremios = cantPremios
         self.cantReconocimientos = cantReconocimientos
-        self.premios = premios
+        self.cantPremios = cantPremios
+        self.premios = premios if premios is not None else {}
 
     def crear_jugador(self):
         self.nombre = input("Ingrese nombre del jugador:")
@@ -27,9 +27,9 @@ class jugFutbol:
         self.asistencias = int(input("Ingrese asistencias del jugador:"))
         self.tarRoja = int(input("Ingrese cantidad de tarjetas rojas del jugador:"))
         self.tarAmarilla = int(input("Ingrese cantidad de tarjetas amarillas del jugador:"))
-        self.cantPremios = int(input("Ingrese cantidad de premios del jugador:"))
         self.cantReconocimientos = int(input("Ingrese cantidad de reconocimientos del jugador:"))
 
+        self.cantPremios = int(input("Ingrese cantidad de premios del jugador:"))
         self.premios = {}  
         for _ in range(self.cantPremios):
             equipo = input("Ingrese el equipo al que le ganó el premio: ")
@@ -39,12 +39,59 @@ class jugFutbol:
         print(f"Los datos de tu jugador son: {self.nombre} de {self.edad} años, juega de {self.posicion} en {self.equipo}, y en {self.pais}, usa el {self.numCamisa}. Marcó {self.goles} goles y {self.asistencias} asistencias en {self.partidos} partidos, le sacaron {self.tarAmarilla} amarillas y {self.tarRoja} rojas. Ganó estos premios: {self.premios}")
 
 
-# Crear jugador1 con información vacía
+    def actualizar_info(self):
+        self.partidos += 1
+        self.goles += int(input("¿Cuántos goles hizo?"))
+        self.asistencias += int(input("¿Cuántos asistencias hizo?"))
+        self.tarRoja += int(input("¿Cuántas tarjetas rojas tuvo?"))
+        self.tarAmarilla += int(input("¿Cuántas tarjetas amarillas tuvo?"))
+        print(f'Datos actualizados: Cantidad de goles actual: {self.goles}, Cantidad de asistencias: {self.asistencias}, Cantidad de amarillas: {self.tarAmarilla}, Cantidad de rojas: {self.tarRoja}, Jugo un total de {self.partidos} partidos.')
+
+    
+    def goles_partido(self):
+        promedio = self.goles / self.partidos
+        print(f'El promedio de gol por partido de {self.nombre} es = {promedio}')
+
+    
+    def es_goleador(self):
+        pos = self.posicion.lower()
+        if pos == "goleador":
+            print("La posición del jugador ingresado es goleador.")
+        else:
+            print(f"La posición del jugador no es goleador, es {self.posicion}")
+
+
+    def agregar_premios(self):
+        ganoPremios = input("Ganó algun premio? Ingrese si/no:")
+        if ganoPremios == "si":
+            premiosCant = int(input("¿Cuántos premios ganó?"))
+            self.cantPremios += premiosCant
+            if premiosCant > 0:
+                for _ in range(premiosCant):
+                    equipo = input("Ingrese el equipo al que le ganó el premio: ")
+                    premio = input(f"Ingrese el premio que ganó al equipo {equipo}: ")
+                    self.premios[equipo] = premio
+            print(f'Sus premios ahora son: {self.premios}')
+        else:
+            print("No ganó ningún premio.")
+
+
+       
+
+
+# crear jg1 con info vacia
 jugador1 = jugFutbol("", 0, "", "", "", 0, 0, 0, 0, 0, 0, 0, {})
 
-# Llamar al método crear_jugador para crear un jugador y almacenarlo en jugador1
-jugador1.crear_jugador()
+# modificar la info vacia llamando la func de crear jugador y almacenando todo en el jugador recien
+#jugador1.crear_jugador()
 
 # Crear jugador2 con información proporcionada
-jugador2 = jugFutbol("Martina", 17, "defensa", "independiente", "belgica", 7, 0, 1, 1, 4, 12 , {"Boca Juniors": "Copa Libertadores", "Real Madrid": "Champions League"})
-print(f"Los datos de tu jugador son: {jugador2.nombre} de {jugador2.edad} años, juega de {jugador2.posicion} en {jugador2.equipo}, y en {jugador2.pais}, usa el {jugador2.numCamisa}. Marcó {jugador2.goles} goles y {jugador2.asistencias} asistencias en {jugador2.partidos} partidos, le sacaron {jugador2.tarAmarilla} amarillas y {jugador2.tarRoja} rojas. Ganó estos premios: {jugador2.premios}")
+jugador2 = jugFutbol("Martina", 17, "Goleador", "independiente", "belgica", 7, 10, 4, 1, 34, 3, 3,premios = {"Barcelona": "Mejor Jugador del Mes", "Real Madrid": "Máximo Goleador de la Temporada", "PSG": "Premio Fair Play"})
+
+# Imprimir los datos del jugador2 con los premios
+print(f"Los datos de tu jugador son: {jugador2.nombre} de {jugador2.edad} años, juega de {jugador2.posicion} en {jugador2.equipo}, y en {jugador2.pais}, usa el {jugador2.numCamisa}. Marcó {jugador2.goles} goles y {jugador2.asistencias} asistencias en {jugador2.partidos} partidos, le sacaron {jugador2.tarAmarilla} amarillas y {jugador2.tarRoja} rojas. Gano estos premios {jugador2.premios}")
+
+#jugador2.actualizar_info()
+jugador2.goles_partido()
+jugador2.agregar_premios()
+jugador2.es_goleador()
